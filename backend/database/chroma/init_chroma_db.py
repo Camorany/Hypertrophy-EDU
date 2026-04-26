49b8c3c5c686990e5b4f4845a7c266562e49fc08
Camorany <Cjlane9090@gmail.com>
2026-04-26 17:21:09 +0100

    text splitter import path change

--- backend/database/chroma/init_chroma_db.py
+++ backend/database/chroma/init_chroma_db.py
@@ -1,6 +1,6 @@
 import os 
 import glob
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai.embeddings import OpenAIEmbeddings
